4f07bea5cdf523ce48249cb04ec481cb8202e6a4
nhuando <donhuanvn96@gmail.com>
2022-05-28 08:44:14 +0700

    fix issues of empty replies of conversation (by Ngoc)

--- backend/python/utils.py
+++ backend/python/utils.py
@@ -12,7 +12,7 @@ DATA_DIR = os.path.join(os.getcwd(), '..', 'data')
 
 def get_tweets_by_user(user, max_tweets=10, start_time=None, end_time=None):
     url = 'https://api.twitter.com/2/tweets/search/recent'
-    query_data = [('query', 'from:' + user), ('max_results', '10')]
+    query_data = [('query', 'from:' + user + " -is:retweet -is:reply"), ('max_results', str(max_tweets))]
     query_url = url + '?' + \
         '&'.join([str(t[0]) + '=' + str(t[1]) for t in query_data])
     response = requests.get(query_url, headers=headers, stream=True)
